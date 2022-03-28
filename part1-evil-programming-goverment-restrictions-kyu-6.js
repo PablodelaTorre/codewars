2075d8d5065dbbd32bc8d37e5aafdb0ae21e775b
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-27 21:27:52 -0300

    otro ejercicio terminado

---
+++ part1-evil-programming-goverment-restrictions-kyu-6.js
@@ -0,0 +1,35 @@
+var anarchy = function () {
+    let array = ["I ","can ","write ","numbers ","like",", "]
+    let ar = ["a"]
+    let arr = ["a","a"]
+    let arra = ["a", "a", "a"]
+    let num = ""
+    
+    arr.forEach(item=>{
+        parseInt(num)
+        num++
+    })
+    array.push(num)
+    array.push(", ")
+    num = ""
+    
+    arr2.forEach(item=>{
+        parseInt(num)
+        num++
+    })
+    array.push(num)
+    array.push(", ")
+    num = ""
+    
+    arr3.forEach(item=>{
+        parseInt(num)
+        num++
+    })
+    array.push(num)
+    array.push(".")
+    console.log(array)
+    console.log(array.join(""))
+    return array.join("");
+}
+
+anarchy()
